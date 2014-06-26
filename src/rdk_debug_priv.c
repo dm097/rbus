e77ae645905779f71189254b165aa3890b30459a
Vinod GM <vinodgm@tataelxsi.co.in>
2014-06-26 18:24:50 +0530

    JIRA: DELIA-4291 : Removed the socket_evn appender related code from rdk_logger, while keeping the stream_env as removing the latter is creating issue in logging
    Change-Id: I63f0abfdbc7bbeafd7565cca6a96e27805fb61a2

--- src/rdk_debug_priv.c
+++ src/rdk_debug_priv.c
@@ -36,7 +36,6 @@
 #include <string.h>
 #include <ctype.h>
 #include <limits.h>   
-#include <sys/socket.h>
 #include <unistd.h>
 #include <sys/syscall.h>   /* For SYS_xxx definitions */
 #include <sys/errno.h>
@@ -85,27 +84,11 @@ extern int global_count;
 
 #define HOSTADDR_STR_MAX 255
 
-/** The default port if one is not supplied via the socket.host file. */
-#define SOCKET_PORT 51400
-
-#define SO_MAX_MSG_SIZE (64*1024)
-#define MAX_COMPUTERNAME_LENGTH 80
-#define INVALID_SOCKET -1
-
-#define SOCKET_DGRAM_SIZE 4096
 
 #define MAX_LOGLINE_LENGTH 4096
 
 
-typedef struct sockaddr SOCKADDR_IN;
-typedef int SOCKET;
-
 static int initLogger(char *category);
-static void socket_open_log(const char* ident, int option);
-static void socket_close_log();
-static void socket_append_msg(char* fmt, ...);
-static void vsocket_append_msg(char* fmt, va_list ap);
-static const char* set_socket_conf_dir(const char* dir);
 static const char* dated_format_nocr(const log4c_layout_t* a_layout,
         const log4c_logging_event_t*a_event);
 static const char* basic_format_nocr(const log4c_layout_t* a_layout,
@@ -117,27 +100,11 @@ static int stream_env_append_open(log4c_appender_t * appender);
 static int stream_env_append(log4c_appender_t* appender, const log4c_logging_event_t* a_event);
 static int stream_env_plus_stdout_append(log4c_appender_t* appender, const log4c_logging_event_t* a_event);
 static int stream_env_close(log4c_appender_t * appender);
-static int socket_env_open(log4c_appender_t * appender);
-static int socket_env_append(log4c_appender_t* appender, const log4c_logging_event_t* a_event);
-static int socket_env_close(log4c_appender_t * appender);
 
 /* GLOBALS */
 
-/** Define SOCKET_CONF_DIR, the default location where socket.host should exist. */
-#ifndef SOCKET_CONF_DIR
-static const char *g_socket_conf_dir = ".";
-#else
-static const char *g_socket_conf_dir = SOCKET_CONF_DIR;
-#endif
-
 static rdk_logger_Bool g_initialized = FALSE;
-static struct sockaddr_in g_sa_logger;
 
-static SOCKET g_socket;
-
-static char g_local_hostname[MAX_COMPUTERNAME_LENGTH + 1];
-static char g_datagramm[SOCKET_DGRAM_SIZE];
-static size_t g_datagramm_size;
 
 /** Global variable used to control logging. */
 uint32_t rdk_g_logControlTbl[RDK_MAX_MOD_COUNT];
@@ -195,9 +162,6 @@ static const log4c_appender_type_t log4c_appender_type_stream_env_plus_stdout =
 static const log4c_appender_type_t log4c_appender_type_stream_env_append_plus_stdout =
 { "stream_env_append_plus_stdout", stream_env_append_open,
         stream_env_plus_stdout_append, stream_env_close, };
-
-static const log4c_appender_type_t log4c_appender_type_socket_env =
-{ "socket_env", socket_env_open, socket_env_append, socket_env_close, };
 
 void rdk_dbg_priv_Init()
 {
@@ -374,93 +338,6 @@ static int parseLogConfig(const char *cfgStr, uint32_t *configEntry,
     return rc;
 }
 
-/*
- * Initialize udp logging support based on the configuration string
- * provided in "debug.ini".
- *
- * Returns FALSE if either the configuration is invalid or any system
- * call used to initialize the networking support fails.
- */
-static rdk_logger_Bool createUDPSocket(const char *cfgStr)
-{
-    long port = 0;
-    char hostaddr[HOSTADDR_STR_MAX + 1];
-    const char *portStr = NULL;
-    char *strEnd = NULL;
-    int status = 0;
-
-    portStr = strchr(cfgStr, ':');
-    if (portStr == NULL)
-    {
-        /* no colon = no port number */
-        printf("*** invalid configuration no colon!\n");
-        return FALSE;
-    }
-
-    memset(hostaddr, 0, sizeof(hostaddr));
-    strncpy(hostaddr, cfgStr, portStr - cfgStr);
-    printf("*** hostaddr = '%s'\n", hostaddr);
-    ++portStr; /**< skip past ':' */
-    if (*portStr == 0)
-    {
-        /** there was a colon but no port specified */
-        printf("*** no port specified\n");
-        return FALSE;
-    }
-
-    port = strtol(portStr, &strEnd, 10);
-    if (port <= 0 || *strEnd != 0 || port >= SHRT_MAX)
-    {
-        /** invalid number, invalid port or has trailing garbage */
-        printf("*** invalid number specified for port or trailing garbage.\n");
-        return FALSE;
-    }
-
-#if 0
-    if (rdk_logger_socketInit() == FALSE)
-    {
-        printf("** socket layer could not be initialized\n");
-        return FALSE;
-    }
-#endif
-
-    dbg_udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (dbg_udpSocket == -1)
-    {
-        /** failed to create socket */
-        printf("*** failed to create socket\n");
-        return FALSE;
-    }
-
-    /** initialise the logging host address */
-
-    (void) memset((uint8_t *) &dbg_logHostAddr, 0, sizeof(dbg_logHostAddr));
-    dbg_logHostAddr.sin_family = AF_INET;
-    status = inet_aton(hostaddr, &(dbg_logHostAddr.sin_addr));
-    if (status == 0)
-    {
-        /** invalid or unknown host name */
-        printf("*** invalid or unknown host\n");
-        return FALSE;
-    }
-
-    printf("*** port number = %li\n", port);
-    dbg_logHostAddr.sin_port = htons((uint16_t) port);
-
-#if 0
-    status = rdk_logger_socketConnect(dbg_udpSocket,
-            (rdk_logger_SocketSockAddr*)&dbg_logHostAddr,
-            sizeof(dbg_logHostAddr));
-    if (status == 0 )
-    {
-        printf("*** error failed to connect socket");
-        return FALSE;
-    }
-#endif
-
-    printf("*** successfully setup logging socket\n");
-    return TRUE;
-}
 
 /*****************************************************************************
  *
@@ -480,14 +357,6 @@ void rdk_dbg_priv_LogControlInit(void)
     uint32_t defaultConfig = 0;
     int mod = 0;
     const char *msg = "";
-
-    /* Configure UDP logging if desired. */
-    envVarValue = rdk_logger_envGet("LOG.RDK.UDP");
-    if (envVarValue != NULL && createUDPSocket(envVarValue) == TRUE)
-    {
-        printf("udp logging enabled!");
-        dbg_logViaUDP = TRUE;
-    }
 
     /** Pre-condition the control table to disable all logging.  This
      * means that if no logging control statements are present in the
@@ -728,267 +597,8 @@ void rdk_debug_priv_log_msg( rdk_LogLevel level,
     }
 }
 
-/*************************Copied from socket_client.c/socket_client.h******************/
 
 
-
-
-
-
-/******************************************************************************
- * set_socket_conf_dir
- *
- * Set the configuration directory for the socket.host file.
- */
-static const char* set_socket_conf_dir(const char* dir)
-{
-    const char *ret = g_socket_conf_dir;
-    g_socket_conf_dir = dir;
-    return ret;
-}
-
-/******************************************************************************
- * init_logger_addr
- *
- * Read configuration file socket.host. This file should contain host address
- * and, optionally, port. Initialize sa_logger. If the configuration file does
- * not exist, use localhost:51400.
- * Returns: 0 - ok, -1 - error.
- */
-static void init_logger_addr()
-{
-#ifdef WIN32
-    char pathname[FILENAME_MAX];
-    char *p;
-    FILE *fd;
-    char host[256];
-    struct hostent * phe;
-
-    memset(&g_sa_logger, 0, sizeof(SOCKADDR_IN));
-    g_sa_logger.sin_family = AF_INET;
-
-    if ('\\' == g_socket_conf_dir[0] || '/' == g_socket_conf_dir[0] || ':' == g_socket_conf_dir[1])
-    {
-        /* Absolute path. */
-        strcpy(pathname, g_socket_conf_dir);
-    }
-    else
-    {
-        /* Relative path. */
-        char *q;
-
-        strcpy(pathname, __argv[0]);
-        if (NULL != (p = strrchr(pathname, '\\')))
-        p++;
-        else
-        goto use_default;
-
-        if (NULL != (q = strrchr(pathname, '/')))
-        q++;
-        else
-        goto use_default;
-
-        if (p < q)
-        *q = 0;
-        else if (p > q)
-        *p = 0;
-        else
-        pathname[0] = 0;
-        strcat(pathname, g_socket_conf_dir);
-    }
-    p = &pathname[strlen(pathname) - 1];
-    if ('\\' != *p && '/' != *p)
-    {
-        p++; *p = '/';
-    }
-    strcpy(++p, "socket.host");
-
-    /* Read destination host name. */
-    fd = fopen(pathname, "r");
-    if (! fd)
-    goto use_default;
-
-    if (NULL == fgets(host, sizeof(host), fd))
-    host[0] = 0;
-    else
-    {
-        p = strchr(host, '\n');
-        if (p)
-        *p = 0;
-        p = strchr(host, '\r');
-        if (p)
-        *p = 0;
-    }
-    fclose(fd);
-
-    p = strchr(host, ':');
-    if (p)
-    *p++ = 0;
-
-    phe = gethostbyname(host);
-    if (! phe)
-    goto use_default;
-
-    memcpy(&g_sa_logger.sin_addr.s_addr, phe->h_addr, phe->h_length);
-
-    if (p)
-    g_sa_logger.sin_port = htons((unsigned short) strtoul(p, NULL, 0));
-    else
-    g_sa_logger.sin_port = htons(SOCKET_PORT);
-    return;
-
-    use_default:
-    g_sa_logger.sin_addr.S_un.S_addr = htonl(0x7F000001);
-#else
-    g_sa_logger.sin_family = AF_INET;
-    g_sa_logger.sin_addr.s_addr = inet_addr("127.0.0.1");
-#endif
-    g_sa_logger.sin_port = htons(SOCKET_PORT);
-}
-
-/******************************************************************************
- * socket_open_log
- *
- * Open connection to system logger.
- */
-static void socket_open_log(const char* ident, int option)
-{
-    int failed = 0;
-#ifdef RI_WIN32_SOCKETS
-    int wsa_initialized = 0;
-    WSADATA wsd;
-    int size;
-#endif
-    struct sockaddr_in sa_local;
-    int n;
-
-    if (g_initialized)
-        return;
-
-    // Parse options here if there are any.
-
-#ifdef RI_WIN32_SOCKETS
-    // Initialize windows sockets
-    if (WSAStartup(MAKEWORD(2, 2), &wsd))
-    goto done;
-    wsa_initialized = 1;
-#endif
-
-    // Get local host name
-    n = sizeof(g_local_hostname);
-    if (gethostname(g_local_hostname, n) == -1)
-        goto done;
-
-    g_socket = INVALID_SOCKET;
-
-    init_logger_addr();
-
-    for (n = 0;; n++)
-    {
-        g_socket = socket(AF_INET, SOCK_DGRAM, 0);
-        if (INVALID_SOCKET == g_socket)
-            goto done;
-
-        memset(&sa_local, 0, sizeof(struct sockaddr_in));
-        sa_local.sin_family = AF_INET;
-        if (bind(g_socket, (struct sockaddr*) &sa_local,
-                sizeof(struct sockaddr)) == 0)
-            break;
-#ifdef RI_WIN32_SOCKETS
-        (void)closesocket(g_socket);
-#else
-        (void) close(g_socket);
-#endif
-        g_socket = INVALID_SOCKET;
-        if (n == 100)
-            goto done;
-    }
-
-    /* Get size of datagramm. */
-#ifdef RI_WIN32_SOCKETS
-    size = sizeof(g_datagramm_size);
-    if (getsockopt(g_socket, SOL_SOCKET, SO_MAX_MSG_SIZE, (char*) &g_datagramm_size, &size))
-    goto done;
-    if (g_datagramm_size - strlen(g_local_hostname) - (ident ? strlen(ident) : 0) < 64)
-    goto done;
-    if (g_datagramm_size > sizeof(g_datagramm))
-    g_datagramm_size = sizeof(g_datagramm);
-#else
-    g_datagramm_size = sizeof(g_datagramm);
-#endif
-
-    failed = 0;
-
-    done: if (failed)
-    {
-#ifdef RI_WIN32_SOCKETS
-        if (g_socket != INVALID_SOCKET) (void)closesocket(g_socket);
-        if (wsa_initialized) WSACleanup();
-#else
-        if (g_socket != INVALID_SOCKET)
-            (void) close(g_socket);
-#endif
-    }
-    g_initialized = !failed;
-}
-
-/******************************************************************************
- * socket_close_log
- *
- * Close descriptor used to write to system logger.
- */
-static void socket_close_log()
-{
-    if (!g_initialized)
-        return;
-#ifdef RI_WIN32_SOCKETS
-    (void)closesocket(g_socket);
-    WSACleanup();
-#else
-    (void) close(g_socket);
-#endif
-    g_initialized = FALSE;
-}
-
-/******************************************************************************
- * socket_append_msg
- *
- * Generate a log message using FMT string and option arguments.
- */
-static void socket_append_msg(char* fmt, ...)
-{
-    va_list ap;
-
-    va_start(ap, fmt);
-    vsocket_append_msg(fmt, ap);
-    va_end(ap);
-}
-
-/******************************************************************************
- * vsocket_append_msg
- *
- * Generate a log message using FMT and using arguments pointed to by AP.
- */
-static void vsocket_append_msg(char* fmt, va_list ap)
-{
-    //char *p;
-    int num_chars;
-    ssize_t numSent;
-
-    if (!g_initialized)
-        return;
-
-    num_chars = vsnprintf(g_datagramm, g_datagramm_size, fmt, ap);
-    g_datagramm[num_chars + 1] = '\0';
-
-    numSent = sendto(g_socket, g_datagramm, strlen(g_datagramm), 0,
-            (struct sockaddr*) &g_sa_logger, sizeof(struct sockaddr));
-    if (numSent < 0)
-    {
-        printf("***** Error occurred while sending log message. *****\n");
-    }
-}
-/*************************End Copied from socket_client.c/socket_client.h******************/
 
 /*************************Copied from ri_log.c******************/
 
@@ -1007,7 +617,6 @@ static int initLogger(char *category)
     (void) log4c_appender_type_set(&log4c_appender_type_stream_env_plus_stdout);
     (void) log4c_appender_type_set(
             &log4c_appender_type_stream_env_append_plus_stdout);
-    (void) log4c_appender_type_set(&log4c_appender_type_socket_env);
     (void) log4c_layout_type_set(&log4c_layout_type_dated_nocr);
     (void) log4c_layout_type_set(&log4c_layout_type_basic_nocr);
     (void) log4c_layout_type_set(&log4c_layout_type_comcast_dated_nocr);
@@ -1327,101 +936,6 @@ static int stream_env_close(log4c_appender_t* appender)
     return fclose(fp);
 }
 
-static int socket_env_open(log4c_appender_t* appender)
-{
-    int retval = 0;
-    char* name = g_strdup(log4c_appender_get_name(appender));
-    int nameLen = strlen(name);
-    char* temp = name;
-    char *varBegin, *varEnd;
-    char* envVar;
-    const int MAX_VAR_LEN = 1024;
-    char newName[MAX_VAR_LEN+1];
-    int newNameLen = 0;
-
-    newName[0] = '\0';
-
-    ///> Parse any environment variables.
-    while ((varBegin = strchr(temp,'$')) != NULL)
-    {
-        ///> Search for opening and closing parens.
-        if (((varBegin - name + 1) >= nameLen) || (*(varBegin+1) != '('))
-        goto parse_error;
-
-        ///> Append characters up to this point to the new name.
-        strncat(newName, temp, varBegin-temp);
-        newNameLen += varBegin-temp;
-        if (newNameLen > MAX_VAR_LEN)
-        goto length_error;
-
-        varBegin += 2; ///> Start of env var name.
-
-        if ((varEnd = strchr(varBegin,')')) == NULL)
-        goto parse_error;
-
-        *varEnd = '\0';
-        if ((envVar = getenv(varBegin)) == NULL)
-        goto parse_error;
-
-        ///> Append env var value to the new name.
-        strncat(newName, envVar, strlen(envVar));
-        newNameLen += strlen(envVar);
-        if (newNameLen >MAX_VAR_LEN)
-        goto length_error;
-
-        temp = varEnd + 1;
-    }
-
-    ///> Append remaining characters.
-    strncat(newName, temp, (name + nameLen) - temp);
-    newNameLen += (name + nameLen) - temp;
-    if (newNameLen >MAX_VAR_LEN)
-    goto length_error;
-
-    ///> Call the socket client implementation.
-    if (strcmp(newName, "socket"))
-    ///> Must be specifying the configuration directory for the socket.host file.
-    (void)set_socket_conf_dir(newName);
-    socket_open_log(name, 0);
-
-    g_free(name);
-
-    return retval;
-
-    parse_error:
-    fprintf(stderr, "*(*(*(*( log4c appender socket_env, %s -- Illegal env var name or format! %s\n",
-            __FUNCTION__, name);
-    (void)fflush(stderr);
-    g_free(name);
-    return -1;
-
-    length_error:
-    fprintf(stderr, "*(*(*(*( log4c appender socket_env, %s -- Path is too long! %s\n",
-            __FUNCTION__, name);
-    (void)fflush(stderr);
-    g_free(name);
-    return -1;
-}
-
-static int socket_env_append(log4c_appender_t* appender, const log4c_logging_event_t* a_event)
-{
-    int retval = 0;
-
-    ///> Call the socket client implementation.
-    socket_append_msg((char*)"%s", a_event->evt_rendered_msg);
-
-    //g_free((void *)a_event->evt_rendered_msg);
-
-    return retval;
-}
-
-static int socket_env_close(log4c_appender_t* appender)
-{
-    ///> Call the socket client implementation.
-    socket_close_log();
-
-    return 0;
-}
 
 /*************************End Copied from ri_log.c******************/
 
