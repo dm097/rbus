114bf4b138ab6bd161c462c13b6edaa7e5369a06
Anand <anand@ccpstb-ae-a052-p.ae.ccp.cable.comcast.com>
2014-07-11 02:52:53 +0000

    RDKDEV-5109 add comcast attribution

--- src/include/rdk_debug_priv.h
+++ src/include/rdk_debug_priv.h
@@ -1,9 +1,9 @@
 /*
  * ============================================================================
- * COMCAST CONFIDENTIAL AND PROPRIETARY
+ * COMCAST C O N F I D E N T I A L AND PROPRIETARY
  * ============================================================================
- * This file and its contents are the intellectual property of Comcast. It may
- * not be used, copied, distributed or otherwise disclosed in whole or in part
+ * This file and its contents are the intellectual property of Comcast.  It may
+ * not be used, copied, distributed or otherwise  disclosed in whole or in part
  * without the express written permission of Comcast.
  * ============================================================================
  * Copyright (c) 2013 Comcast. All rights reserved.
--- src/rdk_logger_util.c
+++ src/rdk_logger_util.c
@@ -1,12 +1,12 @@
 /*
  * ============================================================================
- * COMCAST CONFIDENTIAL AND PROPRIETARY
+ * COMCAST C O N F I D E N T I A L AND PROPRIETARY
  * ============================================================================
- * This file and its contents are the intellectual property of Comcast. It may
- * not be used, copied, distributed or otherwise disclosed in whole or in part
+ * This file and its contents are the intellectual property of Comcast.  It may
+ * not be used, copied, distributed or otherwise  disclosed in whole or in part
  * without the express written permission of Comcast.
  * ============================================================================
- * Copyright (c) 2013 Comcast. All rights reserved.
+ * Copyright (c) 2014 Comcast. All rights reserved.
  * ============================================================================
  */
 
