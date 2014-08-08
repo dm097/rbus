39ce887a84c5a626f7a67518ff82217eec60c478
Anand Mohan <anand_mohan@cable.comcast.com>
2014-08-08 19:12:24 +0000

    RDKDEV-5109 - Add/Update RDK-M attribution

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
+#
+# ============================================================================
+# COMCAST C O N F I D E N T I A L AND PROPRIETARY
+# ============================================================================
+# This file (and its contents) are the intellectual property of Comcast.  It may
+# not be used, copied, distributed or otherwise  disclosed in whole or in part
+# without the express written permission of Comcast.
+# ============================================================================
+# Copyright (c) 2014 Comcast. All rights reserved.
+# ============================================================================
+#
 PWD=`pwd`
 export RDK_DIR=`echo $PWD/..`
 source soc/build/soc_env.sh
--- include/rdk_utils.h
+++ include/rdk_utils.h
@@ -2,7 +2,7 @@
  * ============================================================================
  * COMCAST C O N F I D E N T I A L AND PROPRIETARY
  * ============================================================================
- * This file and its contents are the intellectual property of Comcast.  It may
+ * This file (and its contents) are the intellectual property of Comcast.  It may
  * not be used, copied, distributed or otherwise  disclosed in whole or in part
  * without the express written permission of Comcast.
  * ============================================================================
