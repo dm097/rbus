68edee9d3c23a1f72c5bd0da70996c305c5a8bf5
Balaji Punnuru <balaji_punnuru@cable.comcast.com>
2016-06-13 13:57:19 -0400

    DELIA-13976, DELIA-12744: Added support for journal logging.
    Reason for change: Changed the api to use journal api for logging instead of stdout.
                       Sometimes we have observed that logs were going out of order.
                       We suspect it is happening due to the fact that journal is not
                       timestamping the log in timely manner.
    
    Test Procedure: Build and Verify Logging Works.
    
    Risks: High
    
    Change-Id: Ibc05c2618860eef5c52a32068e2dedce7c0a7bfc
    Signed-off-by: Balaji Punnuru <balaji_punnuru@cable.comcast.com>
    (cherry picked from commit 21bc1cf5ff3659257b6f69e579d84cfb84791181)

--- src/rdk_debug_priv.c
+++ src/rdk_debug_priv.c
@@ -52,6 +52,10 @@
 #include "log4c.h"
 #include <rdk_utils.h>
 
+#ifdef SYSTEMD_JOURNAL
+#include <systemd/sd-journal.h>
+#endif //SYSTEMD_JOURNAL
+
 
 /// Debugging messages are enabled.  Default is enabled (1) and 0 for off.
 static int g_debugEnabled = 1;
@@ -851,11 +855,22 @@ static int stream_env_append_open(log4c_appender_t* appender)
 static int stream_env_append(log4c_appender_t* appender,
         const log4c_logging_event_t* event)
 {
-    int retval;
+    int retval =0;
     FILE* fp = (FILE*)log4c_appender_get_udata(appender);
 
+#ifdef SYSTEMD_JOURNAL
+    if (fp == stdout || fp == stderr)
+    {
+       retval = sd_journal_print(LOG_NOTICE, "%s",event->evt_rendered_msg);
+    }
+    else
+    {
+#endif
     retval = fprintf(fp, "%s", event->evt_rendered_msg);
     (void)fflush(fp);
+#ifdef SYSTEMD_JOURNAL
+    }
+#endif
 
     //free((void *)event->evt_rendered_msg);
 
@@ -865,19 +880,29 @@ static int stream_env_append(log4c_appender_t* appender,
 static int stream_env_plus_stdout_append(log4c_appender_t* appender,
         const log4c_logging_event_t* event)
 {
-    int retval;
+    int retval = 0;
     FILE* fp = (FILE*)log4c_appender_get_udata(appender);
 
+#ifdef SYSTEMD_JOURNAL
+    if (fp != stdout || fp != stderr)
+    {
+       retval = fprintf(fp, "%s", event->evt_rendered_msg);
+    }
+    else
+    {
+       retval = sd_journal_print(LOG_NOTICE, "%s",event->evt_rendered_msg);
+    }
+#else
     retval = fprintf(fp, "%s", event->evt_rendered_msg);
     fprintf(stdout, "%s", event->evt_rendered_msg);
+    (void)fflush(stdout);
+#endif
 
     (void)fflush(fp);
-    (void)fflush(stdout);
-
-    //free((void *)event->evt_rendered_msg);
 
     return retval;
 }
+
 
 static int stream_env_close(log4c_appender_t* appender)
 {
